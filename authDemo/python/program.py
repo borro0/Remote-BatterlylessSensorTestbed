ea69f2ff0778679cf3dde2d0ce8ca0563905f637
Unknown <borisblokland@gmail.com>
2019-02-13 19:22:28 +0100

    Started thinking about how to manage measurements

--- authDemo/python/program.py
+++ authDemo/python/program.py
@@ -2,14 +2,19 @@ import subprocess
 import sys
 import platform
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
-
 binary = sys.argv[1];
 
+#First we flash the MCU with the new firmware
+
+#Note that the flasher should be available in your path for it to work
 if platform.system() == "Windows":
 	subprocess.run(["MSP430Flasher.exe", "-w", binary])
 else:
-	#note that this assumes that the current folder is the nodejs server.js root folder
 	subprocess.run(["MSP430Flasher", "-w", binary])
+
+#We now start the logic analyser
+#Also record the uart output
+
+#We are done with our measurements, store results in file or database
+
 print("i'm done :D")
