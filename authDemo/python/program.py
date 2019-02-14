b74f935c7281e7acf810bea039d40934e5d891e2
Boris Blokland <borisblokland@gmail.com>
2019-02-14 13:28:14 +0100

    Added MSP430 example project

--- authDemo/python/program.py
+++ authDemo/python/program.py
@@ -1,19 +1,40 @@
 import subprocess
 import sys
 import platform
+import threading
 
-binary = sys.argv[1];
+binary = sys.argv[1]; # binary firmware
+time = sys.argv[2]; # time (in seconds)
 
-#First we flash the MCU with the new firmware
+platform = platform.system(); # detect on what operating system we are running
 
-#Note that the flasher should be available in your path for it to work
-if platform.system() == "Windows":
+class LogicAnalyzerThread (threading.Thread):
+   def __init__(self, time):
+      threading.Thread.__init__(self)
+      self.time = time
+   def run(self):
+      if platform == "Windows":
+      	printf("Logic analyzer is currently not supported for windows");
+      else:
+      	print(f"Running logic analyzer for {self.time} seconds.")
+      	# running logic analyzer with demo driver currently
+      	subprocess.run(["sigrok-cli", "--driver", "fx2lafw", "--time", f"{time}s", "-o", "sample.sr"])
+		#sigrok-cli --driver fx2lafw --time 3s -O vcd -o vcd-samplel
+
+# First we flash the MCU with the new firmware, ensure MCU is powered and debugger is connected
+# Note that the flasher should be available in your path for it to work
+if platform == "Windows":
 	subprocess.run(["MSP430Flasher.exe", "-w", binary])
 else:
 	subprocess.run(["MSP430Flasher", "-w", binary])
 
-#We now start the logic analyser
+# We now start the logic analyser
+thread_logic_analyzer = LogicAnalyzerThread(time)
+thread_logic_analyzer.start()
 #Also record the uart output
+
+# Wait for all threads to finish
+thread_logic_analyzer.join()
 
 #We are done with our measurements, store results in file or database
 
